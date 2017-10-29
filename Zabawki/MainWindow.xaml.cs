c9c345b95b620936ff0e48218bd1ba8849995ec8
Patryk Rubaj <patryk.rubaj@gmail.com>
2017-10-29 15:52:41 +0100

    Add project files.

---
+++ Zabawki/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Zabawki
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loadMovementFile_Click(object sender, RoutedEventArgs e)
+        {
+            string filename;
+            string initialDirectory = "H:\\";
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (Directory.Exists(initialDirectory))
+            {
+                fileDialog.InitialDirectory = initialDirectory;
+            }
+            else
+            {
+                fileDialog.InitialDirectory = "C:\\";
+            } 
+            
+            Nullable<bool> result = fileDialog.ShowDialog();
+            if (result == true)
+            {
+                filename = fileDialog.FileName;
+            }
+
+        }
+
+
+    }
+}
---
+++ Zabawki/Model/3DCoordinates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zabawki.Model
+{
+    class _3DCoordinates
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+        private int z { get; set; }
+
+        public _3DCoordinates(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void lol()
+        {
+            x = 3;
+        }
+
+    }
+}
