516f10ecfe91fb85deb0e28c248bf1adeb8483ec
Patryk Rubaj <patryk.rubaj@gmail.com>
2017-10-30 22:46:36 +0100

    more stuff

---
+++ Zabawki/File.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zabawki
+{
+    class File
+    {
+        public File open(string path)
+        {
+
+            return this;
+        }
+    }
+}
--- Zabawki/Model/3DCoordinates.cs
+++ Zabawki/Model/3DCoordinates.cs
@@ -10,11 +10,19 @@ namespace Zabawki.Model
         private int x { get; set; }
         private int y { get; set; }
         private int z { get; set; }
+        private int rotX { get; set; }
+        private int rotY { get; set; }
+        private int rotZ { get; set; }
+        private int rotAngle { get; set; }
 
-        public _3DCoordinates(int x, int y, int z) {
+        public _3DCoordinates(int x, int y, int z, int rotX, int rotY, int rotZ, int rotAngle) {
             this.x = x;
             this.y = y;
             this.z = z;
+            this.rotX = rotX;
+            this.rotY = rotY;
+            this.rotZ = rotZ;
+            this.rotAngle = rotAngle;
         }
 
         public void lol()
