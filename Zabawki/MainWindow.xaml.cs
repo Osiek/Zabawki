d49fc8428cd87e7410743c039fb4d2941680d903
Patryk Rubaj <patryk.rubaj@gmail.com>
2017-11-10 12:43:32 +0100

    added keyboard actions for camera transofrmations

--- Zabawki/MainWindow.xaml.cs
+++ Zabawki/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using Microsoft.Win32;
 using System.IO;
 using System.Windows.Media.Media3D;
 using System.Windows.Media.Animation;
+using Zabawki.Model;
 
 namespace Zabawki
 {
@@ -26,12 +27,27 @@ namespace Zabawki
         private GeometryModel3D mGeometry;
         private bool mDown;
         private Point mLastPos;
-
+        private RotateTransform3D myRotateTransformX;
+        private RotateTransform3D myRotateTransformY;
+        private AxisAngleRotation3D myRotX;
+        private AxisAngleRotation3D myRotY;
+        Transform3DGroup cameraRotationsGroup;
 
         public MainWindow()
         {
             InitializeComponent();
             build3DThing();
+
+            cameraRotationsGroup = new Transform3DGroup();
+
+            myRotX = new AxisAngleRotation3D(new Vector3D(0, 1, 0), 10);
+            myRotateTransformX = new RotateTransform3D(myRotX);
+            myRotY = new AxisAngleRotation3D(new Vector3D(1, 0, 0), 10);
+            myRotateTransformY = new RotateTransform3D(myRotY);
+            cameraRotationsGroup.Children.Add(myRotateTransformX);
+            cameraRotationsGroup.Children.Add(myRotateTransformY);
+
+            camMain.Transform = cameraRotationsGroup;
         }
 
         private void loadMovementFile_Click(object sender, RoutedEventArgs e)
@@ -58,69 +74,9 @@ namespace Zabawki
 
         public void build3DThing()
         {
-
-            MeshGeometry3D mesh = new MeshGeometry3D();
-
-            //front face
-            mesh.Positions.Add(new Point3D(0, 0, 0));
-            mesh.Positions.Add(new Point3D(1, 0, 0));
-            mesh.Positions.Add(new Point3D(0, 1, 0));
-            mesh.Positions.Add(new Point3D(1, 1, 0));
-            //back face
-            mesh.Positions.Add(new Point3D(0, 0, -1));
-            mesh.Positions.Add(new Point3D(1, 0, -1));
-            mesh.Positions.Add(new Point3D(0, 1, -1));
-            mesh.Positions.Add(new Point3D(1, 1, -1));
-
-            //Front face
-            mesh.TriangleIndices.Add(0);
-            mesh.TriangleIndices.Add(3);
-            mesh.TriangleIndices.Add(2);
-            mesh.TriangleIndices.Add(0);
-            mesh.TriangleIndices.Add(1);
-            mesh.TriangleIndices.Add(3);
-
-            //back face
-            mesh.TriangleIndices.Add(4);
-            mesh.TriangleIndices.Add(6);
-            mesh.TriangleIndices.Add(7);
-            mesh.TriangleIndices.Add(7);
-            mesh.TriangleIndices.Add(5);
-            mesh.TriangleIndices.Add(4);
-
-            //Left face
-            mesh.TriangleIndices.Add(2);
-            mesh.TriangleIndices.Add(6);
-            mesh.TriangleIndices.Add(0);
-            mesh.TriangleIndices.Add(0);
-            mesh.TriangleIndices.Add(6);
-            mesh.TriangleIndices.Add(4);
-
-            //right face
-            mesh.TriangleIndices.Add(1);
-            mesh.TriangleIndices.Add(7);
-            mesh.TriangleIndices.Add(3);
-            mesh.TriangleIndices.Add(1);
-            mesh.TriangleIndices.Add(5);
-            mesh.TriangleIndices.Add(7);
-
-            //bottom
-            mesh.TriangleIndices.Add(5);
-            mesh.TriangleIndices.Add(1);
-            mesh.TriangleIndices.Add(4);
-            mesh.TriangleIndices.Add(4);
-            mesh.TriangleIndices.Add(1);
-            mesh.TriangleIndices.Add(0);
-
-            //top
-            mesh.TriangleIndices.Add(2);
-            mesh.TriangleIndices.Add(3);
-            mesh.TriangleIndices.Add(6);
-            mesh.TriangleIndices.Add(6);
-            mesh.TriangleIndices.Add(3);
-            mesh.TriangleIndices.Add(7);
-
-            mGeometry = new GeometryModel3D(mesh, new DiffuseMaterial(Brushes.YellowGreen));
+            var myPlane = new Airplane();
+            mGeometry = myPlane.getPlane();
+            //mGeometry = new GeometryModel3D(mesh, new DiffuseMaterial(Brushes.YellowGreen));
             //mGeometry.Transform = new Transform3DGroup();
             var MyTranslateTransform = new TranslateTransform3D();
             this.RegisterName("myTranslateTransform", MyTranslateTransform);
@@ -131,13 +87,13 @@ namespace Zabawki
             DoubleAnimation x = new DoubleAnimation();
             x.From = -10;
             x.To = 10;
-            x.Duration = TimeSpan.FromSeconds(3);
+            x.Duration = TimeSpan.FromSeconds(10);
             x.RepeatBehavior = RepeatBehavior.Forever;
 
             DoubleAnimation y = new DoubleAnimation();
             y.From = 10;
             y.To = -15;
-            y.Duration = TimeSpan.FromSeconds(3);
+            y.Duration = TimeSpan.FromSeconds(10);
             y.RepeatBehavior = RepeatBehavior.Forever;
 
             Storyboard.SetTargetName(x, "myTranslateTransform");
@@ -150,12 +106,12 @@ namespace Zabawki
             sb.Children.Add(x);
             sb.Children.Add(y);
             sb.Completed += sb_Completed;
-            sb.Begin(this);
+            //sb.Begin(this);
         }
 
         void sb_Completed(object sender, EventArgs e)
         {
-            Console.WriteLine("Completed.");
+            Console.WriteLine("Storyboard completed.");
         }
 
         private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
@@ -183,6 +139,28 @@ namespace Zabawki
         private void Grid_KeyDown(object sender, KeyEventArgs e)
         {
             int klawisz = (int)e.Key;
+
+            if(Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                switch (e.Key)
+                {
+                    case Key.Left:
+                        myRotX.Angle -= 10;
+                        break;
+                    case Key.Right:
+                        myRotX.Angle += 10;
+                        break;
+                    case Key.Up:
+                        myRotY.Angle -= 10;
+                        break;
+                    case Key.Down:
+                        myRotY.Angle += 10;
+                        break;
+                }
+
+                return;
+            }
+
             if (e.Key != Key.Up || e.Key != Key.Down || e.Key != Key.Left || e.Key != Key.Right) System.Console.WriteLine(e.Key.ToString());
 
             switch(e.Key)
---
+++ Zabawki/Model/Airplane.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace Zabawki.Model
+{
+    class Airplane
+    {
+        MeshGeometry3D mesh;
+        GeometryModel3D airplaneModel;
+
+        public Airplane()
+        {
+            mesh = new MeshGeometry3D();
+            makeAirplane();
+        }
+
+        public GeometryModel3D getPlane()
+        {
+            return airplaneModel;
+        }
+
+        private void makeAirplane()
+        {
+            definePoints();
+            makeMesh();
+            airplaneModel = new GeometryModel3D(mesh, new DiffuseMaterial(Brushes.YellowGreen));
+        }
+
+        private void definePoints()
+        {
+            //Lewa sciana samolotu
+            mesh.Positions.Add(new Point3D(0,0,0)); //pkt. 1
+            mesh.Positions.Add(new Point3D(5,0,0)); //pkt. 2
+            mesh.Positions.Add(new Point3D(1,1,0)); //pkt. 3
+            mesh.Positions.Add(new Point3D(5,1,0)); //pkt. 4
+            mesh.Positions.Add(new Point3D(4.5,1,0)); //pkt. 5
+            mesh.Positions.Add(new Point3D(5,1.5,0)); //pkt. 6
+
+            //Prawa sciana samolotu
+            mesh.Positions.Add(new Point3D(0, 0, 1)); //pkt. 1 tył - 7
+            mesh.Positions.Add(new Point3D(5, 0, 1)); //pkt. 2 tył - 8
+            mesh.Positions.Add(new Point3D(1, 1, 1)); //pkt. 3 tył - 9
+            mesh.Positions.Add(new Point3D(5, 1, 1)); //pkt. 4 tył - 10
+            mesh.Positions.Add(new Point3D(4.5, 1, 1)); //pkt. 5 tył - 11
+            mesh.Positions.Add(new Point3D(5, 1.5, 1)); //pkt. 6 tył - 12
+
+            //Lewe skrzydlo
+            mesh.Positions.Add(new Point3D(3, 0, 0)); //pkt. 13
+            mesh.Positions.Add(new Point3D(4, 0, 0)); //pkt. 14
+            mesh.Positions.Add(new Point3D(3, 0, -4)); //pkt. 15
+            mesh.Positions.Add(new Point3D(4, 0, -4)); //pkt. 16
+
+            //Prawe skrzydlo
+            mesh.Positions.Add(new Point3D(3, 0, 1)); //pkt. 17
+            mesh.Positions.Add(new Point3D(4, 0, 1)); //pkt. 18
+            mesh.Positions.Add(new Point3D(3, 0, 5)); //pkt. 19
+            mesh.Positions.Add(new Point3D(4, 0, 5)); //pkt. 20
+        }
+
+        private void makeMesh()
+        {
+            //Lewa strona samolotu
+            mesh.TriangleIndices.Add(0);
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(0);
+            mesh.TriangleIndices.Add(1);
+            mesh.TriangleIndices.Add(3);
+
+            //Prawa strona samolotu
+            mesh.TriangleIndices.Add(6);
+            mesh.TriangleIndices.Add(8);
+            mesh.TriangleIndices.Add(9);
+            mesh.TriangleIndices.Add(9);
+            mesh.TriangleIndices.Add(7);
+            mesh.TriangleIndices.Add(6);
+
+            //Dol
+            mesh.TriangleIndices.Add(0);
+            mesh.TriangleIndices.Add(6);
+            mesh.TriangleIndices.Add(7);
+            mesh.TriangleIndices.Add(7);
+            mesh.TriangleIndices.Add(1);
+            mesh.TriangleIndices.Add(0);
+
+            //Gora
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(9);
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(9);
+            mesh.TriangleIndices.Add(8);
+
+            //Przod
+            mesh.TriangleIndices.Add(0);
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(8);
+            mesh.TriangleIndices.Add(8);
+            mesh.TriangleIndices.Add(6);
+            mesh.TriangleIndices.Add(0);
+
+            //Tyl
+            mesh.TriangleIndices.Add(7);
+            mesh.TriangleIndices.Add(9);
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(7);
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(1);
+
+            //Ogon
+            mesh.TriangleIndices.Add(4);
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(5);
+            mesh.TriangleIndices.Add(10);
+            mesh.TriangleIndices.Add(11);
+            mesh.TriangleIndices.Add(9);
+
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(9);
+            mesh.TriangleIndices.Add(5);
+            mesh.TriangleIndices.Add(5);
+            mesh.TriangleIndices.Add(9);
+            mesh.TriangleIndices.Add(11);
+
+            //mesh.TriangleIndices.Add(4);
+            //mesh.TriangleIndices.Add(11);
+            //mesh.TriangleIndices.Add(10);
+            //mesh.TriangleIndices.Add(4);
+            //mesh.TriangleIndices.Add(5);
+            //mesh.TriangleIndices.Add(11);
+        }
+    }
+}
